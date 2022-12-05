5f59fedd7ce0208be0ddd00676cb359416ca7f70
twinsant <twinsant@gmail.com>
2022-12-05 15:24:08 +0800

    Add: MinIO demo.

--- cmd/root.go
+++ cmd/root.go
@@ -4,13 +4,12 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"io"
+	"context"
 	"log"
 	"os"
 
-	"github.com/sony/sonyflake"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/spf13/cobra"
 )
 
@@ -28,33 +27,79 @@ var rootCmd = &cobra.Command{
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		// sha256
-		f, err := os.Open(args[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
+		// f, err := os.Open(args[0])
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// defer f.Close()
 
-		h := sha256.New()
-		if _, err := io.Copy(h, f); err != nil {
-			log.Fatal(err)
-		}
+		// h := sha256.New()
+		// if _, err := io.Copy(h, f); err != nil {
+		// 	log.Fatal(err)
+		// }
 
-		fmt.Printf("%x\n", h.Sum(nil))
+		// fmt.Printf("%x\n", h.Sum(nil))
 
 		// Sonyflake Id
-		var st sonyflake.Settings
-		st.CheckMachineID = fakeMachineID
-		sf := sonyflake.NewSonyflake(st)
-		if sf == nil {
-			log.Fatal("New Sonyflake failed!")
-		}
+		// var st sonyflake.Settings
+		// st.CheckMachineID = fakeMachineID
+		// sf := sonyflake.NewSonyflake(st)
+		// if sf == nil {
+		// 	log.Fatal("New Sonyflake failed!")
+		// }
 
-		id, err := sf.NextID()
+		// id, err := sf.NextID()
+		// if err != nil {
+		// 	log.Fatal("NextID failed!")
+		// }
+
+		// fmt.Println(id)
+
+		// Minio
+		ctx := context.Background()
+		endpoint := "s3.tikoly.com"
+		// FIXME: DO NOT commit!!!!
+		accessKeyID := "ZaRTBCf2g4ZMGVgu"
+		secretAccessKey := "xCHRw9vv1etAUx0pABvRQiDLnF4euowj"
+		useSSL := true
+
+		// Initialize minio client object.
+		minioClient, err := minio.New(endpoint, &minio.Options{
+			Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+			Secure: useSSL,
+		})
 		if err != nil {
-			log.Fatal("NextID failed!")
+			log.Fatalln(err)
 		}
 
-		fmt.Println(id)
+		// Make a new bucket called mymusic.
+		bucketName := "tiktag"
+
+		err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{})
+		if err != nil {
+			// Check to see if we already own this bucket (which happens if you run this twice)
+			exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+			if errBucketExists == nil && exists {
+				log.Printf("We already own %s\n", bucketName)
+			} else {
+				log.Fatalln(err)
+			}
+		} else {
+			log.Printf("Successfully created %s\n", bucketName)
+		}
+
+		// Upload the zip file
+		objectName := "demo.jpg"
+		filePath := "./demo.jpg"
+		contentType := "image/jpeg"
+
+		// Upload the zip file with FPutObject
+		info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
 	},
 }
 
