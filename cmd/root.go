b0dc73c138ecdc08caf0dfa705e2644a01a53ba7
twinsant <twinsant@gmail.com>
2022-12-06 14:36:52 +0800

    Add: immudb demo.

--- cmd/root.go
+++ cmd/root.go
@@ -4,17 +4,25 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"context"
-	"log"
 	"os"
 
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/codenotary/immudb/pkg/client"
+	// immugorm "github.com/codenotary/immugorm"
+	immudb "github.com/0ctanium/gorm-immudb"
+
 	"github.com/spf13/cobra"
+	"gorm.io/gorm"
 )
 
 func fakeMachineID(uint16) bool {
 	return true
+}
+
+type Product struct {
+	ID     int `gorm:"primarykey"`
+	Code   string
+	Price  uint
+	Amount uint
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -56,50 +64,116 @@ var rootCmd = &cobra.Command{
 		// fmt.Println(id)
 
 		// Minio
-		ctx := context.Background()
-		endpoint := "s3.tikoly.com"
-		// FIXME: DO NOT commit!!!!
-		accessKeyID := "ZaRTBCf2g4ZMGVgu"
-		secretAccessKey := "xCHRw9vv1etAUx0pABvRQiDLnF4euowj"
-		useSSL := true
+		// ctx := context.Background()
+		// endpoint := "s3.tikoly.com"
+		// // FIXME: DO NOT commit!!!!
+		// accessKeyID := "ZaRTBCf2g4ZMGVgu"
+		// secretAccessKey := "xCHRw9vv1etAUx0pABvRQiDLnF4euowj"
+		// useSSL := true
 
-		// Initialize minio client object.
-		minioClient, err := minio.New(endpoint, &minio.Options{
-			Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-			Secure: useSSL,
-		})
+		// // Initialize minio client object.
+		// minioClient, err := minio.New(endpoint, &minio.Options{
+		// 	Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		// 	Secure: useSSL,
+		// })
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+
+		// // Make a new bucket called mymusic.
+		// bucketName := "tiktag"
+
+		// err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{})
+		// if err != nil {
+		// 	// Check to see if we already own this bucket (which happens if you run this twice)
+		// 	exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+		// 	if errBucketExists == nil && exists {
+		// 		log.Printf("We already own %s\n", bucketName)
+		// 	} else {
+		// 		log.Fatalln(err)
+		// 	}
+		// } else {
+		// 	log.Printf("Successfully created %s\n", bucketName)
+		// }
+
+		// // Upload the zip file
+		// objectName := "demo.jpg"
+		// filePath := "./demo.jpg"
+		// contentType := "image/jpeg"
+
+		// // Upload the zip file with FPutObject
+		// info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+
+		// log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+
+		// immudb
+		opts := client.DefaultOptions()
+
+		opts.Username = "immudb"
+		opts.Password = "immudb"
+		opts.Database = "defaultdb"
+		opts.HealthCheckRetries = 10
+
+		// db, err := gorm.Open(immugorm.OpenWithOptions(opts, &immugorm.ImmuGormConfig{Verify: false}), &gorm.Config{
+		// 	Logger: logger.Default.LogMode(logger.Info),
+		// })
+		db, err := gorm.Open(immudb.New(immudb.Config{
+			DSN:                "immudb://immudb:immudb@127.0.0.1:3322/defaultdb", // data source name, refer https://docs.immudb.io/master/develop/sqlstdlib.html
+			DefaultVarcharSize: 256,                                               // add default size for string fields, not a primary key, no index defined and don't have default values
+			DefaultBlobSize:    256,                                               // add default size for bytes fields, not a primary key, no index defined and don't have default values
+			DisableDeletion:    true,                                              // disable row deletion, which not supported before ImmuDB 1.2
+		}), &gorm.Config{})
 		if err != nil {
-			log.Fatalln(err)
+			panic(err)
 		}
 
-		// Make a new bucket called mymusic.
-		bucketName := "tiktag"
-
-		err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{})
+		// Migrate the schema
+		err = db.AutoMigrate(&Product{})
 		if err != nil {
-			// Check to see if we already own this bucket (which happens if you run this twice)
-			exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
-			if errBucketExists == nil && exists {
-				log.Printf("We already own %s\n", bucketName)
-			} else {
-				log.Fatalln(err)
-			}
-		} else {
-			log.Printf("Successfully created %s\n", bucketName)
+			panic(err)
+		}
+		// Create
+		err = db.Create(&Product{Code: "D43", Price: 100, Amount: 500}).Error
+		if err != nil {
+			panic(err)
+		}
+		// Read
+		var product Product
+		// find just created one
+		err = db.First(&product).Error
+		if err != nil {
+			panic(err)
+		}
+		// find product with code D42
+		err = db.First(&product, "code = ?", "D43").Error
+		if err != nil {
+			panic(err)
+		}
+		// Update - update product's price to 200
+		err = db.Model(&product).Update("Price", 888).Error
+		if err != nil {
+			panic(err)
 		}
 
-		// Upload the zip file
-		objectName := "demo.jpg"
-		filePath := "./demo.jpg"
-		contentType := "image/jpeg"
-
-		// Upload the zip file with FPutObject
-		info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+		// Update - update multiple fields
+		err = db.Model(&product).Updates(Product{Price: 200, Code: "F42"}).Error
 		if err != nil {
-			log.Fatalln(err)
+			panic(err)
 		}
 
-		log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+		err = db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"}).Error
+		if err != nil {
+			panic(err)
+		}
+
+		// Delete - delete product
+		// err = db.Delete(&product, product.ID).Error
+		// if err != nil {
+		// 	panic(err)
+		// }
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,28 +3,67 @@ module github.com/twinsant/tiktag
 go 1.19
 
 require (
+	github.com/0ctanium/gorm-immudb v0.1.0 // indirect
+	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
+	github.com/aead/chacha20poly1305 v0.0.0-20201124145622-1a5aba2a8b29 // indirect
+	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/codenotary/immudb v1.4.1 // indirect
+	github.com/codenotary/immugorm v0.0.0-20220117103200-5d136efdb292 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/minio/minio-go v6.0.14+incompatible // indirect
 	github.com/minio/minio-go/v7 v7.0.45 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/o1egl/paseto v1.0.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.12.2 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/rs/xid v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/sony/sonyflake v1.1.0 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.12.0 // indirect
+	github.com/subosito/gotenv v1.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
+	google.golang.org/grpc v1.46.2 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.6 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.24.2 // indirect
 )
